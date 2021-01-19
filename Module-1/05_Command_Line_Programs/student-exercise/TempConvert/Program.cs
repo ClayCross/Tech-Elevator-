a6c52adc5ca654fcefd0504fc6039b3d34f5af11
Clayton Cross <crosscribbs@gmail.com>
2021-01-19 00:12:43 -0500

    Temp Convert Homework

--- Module-1/05_Command_Line_Programs/student-exercise/TempConvert/Program.cs
+++ Module-1/05_Command_Line_Programs/student-exercise/TempConvert/Program.cs
@@ -7,6 +7,21 @@ namespace TempConvert
         static void Main(string[] args)
         {
             Console.WriteLine("What's the current Tempature?");
+            string temp = Console.ReadLine();
+
+            Console.WriteLine("Is the temperature in (C)elsius, or (F)ahrenheit?");
+            string index = Console.ReadLine();
+
+            if (index == "C")
+            {
+                double conversion = double.Parse(temp) * 1.8 + 32;
+                Console.WriteLine(temp + "C is " + conversion + "in fahrenheiht");
+            }
+            else if (index == "F")
+            {
+                double conversion = (double.Parse(temp) - 32) / 1.8;
+                Console.WriteLine(temp + "F is " + conversion + "in celsius");
+            }
 
 
 
