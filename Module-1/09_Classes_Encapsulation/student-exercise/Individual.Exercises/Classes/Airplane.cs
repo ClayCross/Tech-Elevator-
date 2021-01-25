1f6a489dfe1268662038767710ff87334e473094
Clayton Cross <crosscribbs@gmail.com>
2021-01-24 19:36:06 -0500

    Encapsulation Homework

--- Module-1/09_Classes_Encapsulation/student-exercise/Individual.Exercises/Classes/Airplane.cs
+++ Module-1/09_Classes_Encapsulation/student-exercise/Individual.Exercises/Classes/Airplane.cs
@@ -3,6 +3,57 @@
     public class Airplane
     {
 
-        
+        public string PlaneNumber { get; }
+
+
+        public int BookedFirstClassSeats { get; private set; }
+
+
+        public int AvailableFirstClassSeats
+        {
+            get { return TotalFirstClassSeats - BookedFirstClassSeats; }
+        }
+
+        public int TotalFirstClassSeats { get; }
+
+        public int BookedCoachSeats { get; private set; }
+
+        public int AvailableCoachSeats
+        {
+            get { return TotalCoachSeats - BookedCoachSeats; }
+        }
+
+        public int TotalCoachSeats { get; }
+
+        public Airplane(string planeNumber, int totalFirstClassSeats, int totalCoachSeats)
+        {
+            this.PlaneNumber = planeNumber;
+            this.TotalFirstClassSeats = totalFirstClassSeats;
+            this.TotalCoachSeats = totalCoachSeats;
+        }
+
+        public bool ReserveSeats(bool firstClass, int totalNumberOfSeats)
+        {
+            if (firstClass)
+            {
+                if (totalNumberOfSeats > AvailableFirstClassSeats)
+                {
+                    return false;
+                }
+
+                BookedFirstClassSeats += totalNumberOfSeats;
+            }
+            else
+            {
+                if (totalNumberOfSeats > AvailableCoachSeats)
+                {
+                    return false;
+                }
+
+                BookedCoachSeats += totalNumberOfSeats;
+            }
+
+            return true;
+        }
     }
 }
--- Module-1/09_Classes_Encapsulation/student-exercise/Individual.Exercises/Classes/Television.cs
+++ Module-1/09_Classes_Encapsulation/student-exercise/Individual.Exercises/Classes/Television.cs
@@ -3,6 +3,78 @@
     public class Television
     {
 
+        public bool IsOn { get; private set; }
+
+        public int CurrentChannel { get; private set; } = 3;
+
+
+        public int CurrentVolume { get; private set; } = 2;
+
+
+        public void TurnOn()
+        {
+            IsOn = true;
+            CurrentVolume = 2;
+        }
+
+        public void TurnOff()
+        {
+            IsOn = false;
+        }
+       
+        public void ChangeChannel(int newChannel)
+        {
+            if (IsOn && newChannel > 3 && newChannel < 18)
+            {
+                CurrentChannel = newChannel;
+            }
+        }
+
+        public void ChannelUp()
+        {
+            if (IsOn)
+            {
+                if (CurrentChannel == 18)
+                {
+                    CurrentChannel = 3;
+                }
+                else
+                {
+                    CurrentChannel++;
+                }
+            }
+        }
+
+        public void ChannelDown()
+        {
+            if (IsOn)
+            {
+                if (CurrentChannel == 3)
+                {
+                    CurrentChannel = 18;
+                }
+                else
+                {
+                    CurrentChannel--;
+                }
+            }
+        }
+
+        public void RaiseVolume()
+        {
+            if (IsOn && CurrentVolume < 10)
+            {
+                CurrentVolume++;
+            }
+        }
+
+        public void LowerVolume()
+        {
+            if (IsOn && CurrentVolume > 0)
+            {
+                CurrentVolume--;
+            }
+        }
 
     }
 }
