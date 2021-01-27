27b106a1d146ba6fee37f5b1093ce2f60e614419
Clayton Cross <crosscribbs@gmail.com>
2021-01-26 21:46:08 -0500

    Inheritance Homework

--- Module-1/11_Inheritance/student-exercise/BankTellerExercise/Classes/BankAccount.cs
+++ Module-1/11_Inheritance/student-exercise/BankTellerExercise/Classes/BankAccount.cs
@@ -1,7 +1,42 @@
-﻿namespace BankTellerExercise.Classes
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankTellerExercise.Classes
 {
     public class BankAccount
     {
-        
+        public string accountHolderName  { get; private set; }
+
+        public string accountNumber {get; set;}
+
+        public decimal Balance { get; private set; }
+
+        public BankAccount(string accountHolderName, string accountNumber)
+        {
+            this.accountHolderName = accountHolderName;
+            this.accountNumber = accountNumber;
+            this.Balance = 0M;
+        }
+
+        public BankAccount(string accountHolderName, string accountNumber, decimal balance)
+        {
+            this.accountHolderName = accountHolderName;
+            this.accountNumber = accountNumber;
+            this.Balance = balance;
+        }
+        public decimal Deposit(decimal amountToDeposit)
+        {
+            Balance = Balance + amountToDeposit;
+            return Balance;
+        }
+        public virtual decimal Withdraw(decimal amountToWithdraw)
+        {
+            Balance = Balance - amountToWithdraw;
+            return Balance;
+        }
+
     }
 }
--- Module-1/11_Inheritance/student-exercise/BankTellerExercise/Classes/SavingsAccount.cs
+++ Module-1/11_Inheritance/student-exercise/BankTellerExercise/Classes/SavingsAccount.cs
@@ -1,7 +1,37 @@
-﻿namespace BankTellerExercise.Classes
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankTellerExercise.Classes
 {
-    public class SavingsAccount
+    public class SavingsAccount : BankAccount
     {
-        
+        public SavingsAccount(string accountHolderName, string accountNumber, decimal balance)
+            : base(accountHolderName, accountNumber, balance)
+        {
+        }
+
+        public SavingsAccount(string accountHolderName, string accountNumber)
+            : base(accountHolderName, accountNumber)
+        { 
+        }
+
+        public override decimal Withdraw(decimal amountToWithdraw)
+        {
+            decimal serviceCharge = (Balance - amountToWithdraw) <= 150 ? 2.00M : 0;
+
+            if (Balance >= (amountToWithdraw + serviceCharge))
+            {
+                return base.Withdraw(amountToWithdraw + serviceCharge);
+                
+            }
+            else
+            {
+                return Balance;
+            }
+
+        }
     }
 }
