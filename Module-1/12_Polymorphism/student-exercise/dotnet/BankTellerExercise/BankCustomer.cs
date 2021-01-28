060e1999ee8e571c3a908aa5ebfe97caaefbe8f3
Clayton Cross <crosscribbs@gmail.com>
2021-01-28 07:52:48 -0500

    Polymorphism Homework

---
+++ Module-1/12_Polymorphism/student-exercise/dotnet/BankTellerExercise/BankCustomer.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BankTellerExercise
+{
+    class BankCustomer
+    {
+
+        List<IAccountable> accountables = new List<IAccountable> { };
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string PhoneNumber { get; set; }
+        public bool IsVip
+        {
+            get
+            {
+                int sum = 0;
+                foreach (IAccountable account in accountables)
+                {
+                    sum += account.Balance;
+                }
+                return (sum >= 25000);
+            }
+        }
+
+        public void AddAccount(IAccountable newAccount)
+        {
+            accountables.Add(newAccount);
+        }
+
+        public IAccountable[] GetAccounts()
+        {
+            return accountables.ToArray();
+        }
+    }
+}
---
+++ Module-1/12_Polymorphism/student-exercise/dotnet/BankTellerExercise/CreditCardAccount.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BankTellerExercise
+{
+    class CreditCardAccount : IAccountable
+    {
+        public int Balance { get; set; }
+        public int Debt { get { return -1 * Balance; } }
+        public string AccountHolderName { get; }
+        public string AccountNumber { get; }
+
+        public CreditCardAccount(string accountHolderName, string accountNumber)
+        {
+            this.AccountHolderName = accountHolderName;
+            this.AccountNumber = accountNumber;
+        }
+
+        public int Pay(int amountToPay)
+        {
+            Balance += amountToPay;
+            return Debt;
+        }
+
+        public int Charge(int amountToCharge)
+        {
+            Balance -= amountToCharge;
+            return Balance;
+        }
+    }
+}
