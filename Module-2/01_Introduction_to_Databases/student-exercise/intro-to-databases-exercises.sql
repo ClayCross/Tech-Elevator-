6377fb522414604ffa0158be09e9952ffb76db65
Clayton Cross <crosscribbs@gmail.com>
2021-02-10 00:33:14 -0500

    Intro To DataBase

--- Module-2/01_Introduction_to_Databases/student-exercise/intro-to-databases-exercises.sql
+++ Module-2/01_Introduction_to_Databases/student-exercise/intro-to-databases-exercises.sql
@@ -4,29 +4,42 @@
 --   GNP is expressed in millions of US Dollars
 --   The value immediately after the problem statement is the expected number of rows that should be returned by the query.
 
--- 1. The name and population of all cities in Ontario, Canada (27 rows)
-
+-- 1. The name and population of all cities in Ontario, Canada (27 row
+ select name, population from City where district = 'Ontario'
 -- 2. The name and population of all cities in Montana (1 row)
-
+   select Name,Population from City where District = 'Montana'
 -- 3. The name, form of government, and head of state of all countries in Europe (46 rows)
-
+select Name, GovernmentForm, HeadOfState from Country where  Continent = 'Europe'
 -- 4. The name, population, surface area, and average life expectancy of all countries in Asia (51 rows)
-
+ select Name, Population, SurfaceArea, LifeExpectancy FROM Country where Continent = 'Asia'
 -- 5. The name, country code, and population of all cities with a population greater than 8 million people (10 rows)
-
+select Name, CountryCode, Population from City where population > 8000000
 -- 6. The name, country code, and population of all cities with a population less than one thousand people (11 rows)
-
+select name, CountryCode, Population from City where population < 1000
 -- 7. The name, continent, and GNP of all countries with a GNP greater than one trillion dollars (6 rows)
-
+select name, Continent, GNP from Country where gnp > 1000000
 -- 8. The name, continent, population, GNP, and average life expectancy of all countries with an average life expectancy greater than 80 years (5 rows)
-
+ select name, Continent, Population, GNP, LifeExpectancy from Country where LifeExpectancy> 80
 -- 9. The name and population of all cities in the USA with a population of greater than 1 million people (9 rows)
-
+select name, population
+FROM city
+WHERE countrycode = 'USA'
+AND population > 1000000
 -- 10. The name and population of all cities in China with a population of greater than 1 million people (35 rows)
-
+select name, population
+from city
+where countrycode = 'CHN'
+AND population > 1000000;
 -- 11. The name and region of all countries in North or South America (51 rows)
+select name, region
+from country
+where continent = 'North America'
+OR continent = 'South America';
 
 -- 12. The name, continent, and head of state of all countries whose form of government is a monarchy (43 rows)
+select name, continent, headofstate
+from country
+where governmentform LIKE '%Monarchy%';
 
 -- 13. The name of all cities in the USA with a population between 1 million and 2 million people (6 rows) 
 
@@ -35,6 +48,10 @@
 -- 15. The name, population, and GNP of all countries with a GNP greater than $1 trillion dollars and a population of less than 100 million people (4 rows)
 
 -- 16. The name and population of all cities in Texas that have a population of greater than 1 million people (3 rows)
+select name, population
+from city
+where district = 'Texas'
+and population > 1000000;
 
 -- 17. The name and average life expectancy of all countries on the continent of Oceania (28 rows)
 
@@ -46,4 +63,4 @@
 
 -- 21. The per capita GNP (i.e. GNP divided by population) in US Dollars of all countries in Europe (46 rows)
 
--- 22. The number of years since independence for all countries that have a year of independence (192 rows)
+-- 22. The number of years since independence for all countries that have a year of independence (192 rows) 
