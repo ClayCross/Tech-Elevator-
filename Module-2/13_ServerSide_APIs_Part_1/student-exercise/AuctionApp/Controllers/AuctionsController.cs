b540394e5d3dcb65974b60dde19773ef68f7a936
Clayton Cross <crosscribbs@gmail.com>
2021-02-25 23:09:09 -0500

    Server Side API 1

--- Module-2/13_ServerSide_APIs_Part_1/student-exercise/AuctionApp/Controllers/AuctionsController.cs
+++ Module-2/13_ServerSide_APIs_Part_1/student-exercise/AuctionApp/Controllers/AuctionsController.cs
@@ -22,5 +22,39 @@ namespace AuctionApp.Controllers
                 dao = auctionDao;
             }
         }
+        [HttpGet("/auctions")]
+        public List<Auction> GetAllAuctions(string title_like = "")
+
+        {
+            if ( title_like == "" ){
+                return dao.List();
+            }
+            else
+            {
+                 return dao.SearchByTitle(title_like);
+            }
+        }
+
+        [HttpGet("/auctions/{id}")]
+        public Auction GetAuction(int id)
+        {
+            Auction auction = dao.Get(id);
+
+            if (auction != null)
+            {
+                return auction;
+            }
+
+            return null;
+        }
+
+        [HttpPost("/auctions")]
+        public ActionResult<Auction> CreateReservation(Auction newAuction)
+        {
+            Auction auction = dao.Create(newAuction);
+
+            return Created($"/auctions/{auction.Id }", auction);
+        }
+
     }
 }
