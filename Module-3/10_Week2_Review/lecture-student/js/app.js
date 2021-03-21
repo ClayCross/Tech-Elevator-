cba576a044538f2bb10bf28e1f92f0949d8ea606
Clayton Cross <crosscribbs@gmail.com>
2021-03-21 19:26:08 -0400

    Event Handling Shopping List

--- Module-3/10_Week2_Review/lecture-student/js/app.js
+++ Module-3/10_Week2_Review/lecture-student/js/app.js
@@ -2,6 +2,7 @@
  *  Math Practice Notes:
 
     We need to store this information (variables):
+
     * Configurable: 
     *       Total Number of questions (10)
     *       Lowest operand value (0)
@@ -12,9 +13,83 @@
     * An array of possible answers, including the correct one.
 
     We need to handle these events:
+
     * User clicks on an answer
     * User clicks on Start Over
+    * 
+    * 
+    * We need the following functionality:
+    * Add event handlers for selecting the answer, and for the start over.
+    * Score the user's answer and increment the score if correct
+    * Start the game (zero score,Reset the current question to 1)
+    * Create the next question
+    * Generate two random operands for the next question
+    * Generate four possible answers, shuffled , including the correct answer
+    * Display the next question
+    * Take the Js Question object and update screen elements with the questions values
+    * Grade the user's answer and increment the score if correct
+    * Go to the next question if there ar more 
+    * or End the game if user has answered all questions
+    * End the game 
+    * Hide the question and possible answers 
+    * 
+    * 
 *****************************************************/
+
+const NUMBER_OF_QUESTIONS_PER_GAME = 3;
+const MIN_OPERAND_VALUE = 0;
+const MAX_OPERAND_VALUE =9;
+
+
+
+//Temporary code
+for (let i = 0; i < 5; i++)
+{
+    console.log(generateQuestion());
+}
+
+
+
+/**
+ * Create the next question
+ * Generate two random operands for the next question
+ * Generate four possible answers, shuffled, including the correct answer
+ * 
+ * 
+ * 
+ */
+function generateQuestion(){
+let operand1 = getRandomInteger(MIN_OPERAND_VALUE, MAX_OPERAND_VALUE);
+let operand2 = getRandomInteger(MIN_OPERAND_VALUE, MAX_OPERAND_VALUE);
+
+let possibleAnswers = [];
+
+// First put the correct answer in the array
+possibleAnswers.push(operand1 * operand2);
+
+// Then generate three more answers randomly 
+for (let i = 1; i <= 3; i++){
+    while (possibleAnswers[i] === undefined){   
+    let randomAnswer = getRandomInteger(MIN_OPERAND_VALUE * MIN_OPERAND_VALUE, MAX_OPERAND_VALUE * MAX_OPERAND_VALUE);
+    if ( !possibleAnswers.includes(randomAnswer)){
+        possibleAnswers[i] = randomAnswer;
+    }
+  }
+
+}
+
+return {
+    operand1: operand1,
+    operand2: operand2,
+    possibleAnswers: possibleAnswers
+}
+
+
+
+
+
+
+
 
 
 
