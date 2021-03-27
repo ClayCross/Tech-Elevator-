332cfaf938632a0822513c70a168350c1d3c6afb
Clayton Cross <crosscribbs@gmail.com>
2021-03-27 14:50:18 -0400

    Intro To Vue Homework

---
+++ Module-3/11_Introduction_to_Vue_and_Data_Binding/vue-product-review/src/components/ProductReview.vue
@@ -0,0 +1,27 @@
+<template>
+<div class=main>
+<h2>Product Reviews for {{name}}</h2>
+<p class="description">{{description}}</p>
+    </div>
+  
+</template>
+
+<script>
+export default {
+    name: "product-review",
+    // data() is a fuction that returns an object full of oroperties
+    data(){
+        return{
+            name: `Cigar Parties for Dummies`,
+            description: `Host and plan the perfect cigar party for all your squirelly friends`,
+        };
+
+    },
+
+
+}
+</script>
+
+<style scoped>
+
+</style>
